36b81bbdcfbfb78c77e0ad485206ecf658b6b99c
Adriano Almeida Goncalves <adriano.ag@poli.ufrj.br>
2023-03-29 00:16:51 -0300

    Exercicios 7, 8, 9, 10 e 11

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio7.js
@@ -0,0 +1,18 @@
+let nota = 51
+
+if (nota < 0 && nota > 100) {
+    console.log("nota inválida")
+} else if (nota <= 100 && nota >= 90) {
+    console.log("nota A")
+} else if (nota <= 90 && nota >= 80) {
+    console.log("nota B")
+} else if (nota <= 80 && nota >= 70) {
+    console.log("nota C")
+} else if (nota <= 70 && nota >= 60) {
+    console.log("nota D")
+} else if (nota <= 60 && nota >= 50) {
+    console.log("nota E")
+} else {
+    console.log("Nota F")
+}
+
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio9.js
@@ -0,0 +1,9 @@
+let a = 6;
+let b = 100;
+let c = 1983224449;
+
+if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
+    console.log(true)
+} else {
+    console.log(false)
+}
